371e6636e042dccd311cd065ef52778f16808074
SunboyTPB <107842414+SunboyTPB@users.noreply.github.com>
2023-11-05 14:49:56 +0800

    main

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import { Router, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 
 function App() {
   return (
-    <Router>
+    <Routes>
       <Route path='/' element={<Home />} />
-    </Router>
+    </Routes>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Home() {
-
+    return (
+        <>
+        <h1>Hello World! This is the project from React.js!</h1>
+        </>
+    )
 }
 
 export default Home;
