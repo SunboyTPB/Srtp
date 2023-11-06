b774c331c325cf79c3e21552b22d6a64b1a7a407
SunboyTPB <107842414+SunboyTPB@users.noreply.github.com>
2023-11-06 21:03:07 +0800

    main

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,21 @@
+import { Button } from "@mui/material";
 import React from "react";
 
 function Home() {
     return (
         <>
-        <h1>Hello World! This is the project from React.js!</h1>
+        <div className="home">
+            <div className="logo">
+                <h1>SRTP项目组题目测试</h1>
+            </div>
+            <div className="buttons">
+                <Button variant="contained">Android</Button>
+                <Button variant="contained">iOS</Button>
+            </div>
+            <div className="qrcode">
+                
+            </div>
+        </div>
         </>
     )
 }
