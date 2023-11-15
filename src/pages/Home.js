ca4a3ed23b55a77ebcfaf8d625957cca00dbee52
SunboyTPB <107842414+SunboyTPB@users.noreply.github.com>
2023-11-15 20:21:15 +0800

    main

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import { Button, List, ListSubheader,  ListItemButton, ListItemText } from "@mui/material";
+import { Button, List, ListSubheader,  ListItemButton, ListItemText, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, TextField } from "@mui/material";
 import {React, useState} from "react";
 import "./Theme.css";
 
@@ -7,18 +7,28 @@ function Home() {
     const chooseforcontent = (event) => {
         setContentTitle(event.target.id);
     }
+
+    const [openLogin, setOpenLogin] = useState(false);
+    const handlecloseLogin = () => {
+        setOpenLogin(false);
+    }
+
+    const handleopenLogin = () => {
+        setOpenLogin(true);
+    }
     return (
         <>
         <div className="home">
-            <div className="login">
-                
+            <div className="login" style={{"paddingLeft": "10px", "paddingTop": "10px"}}>
+                <p onClick={handleopenLogin} className="link" style={{"width": "50px"}}>登录</p>
             </div>
-            <div className="logo">
+            <div className="logo" style={{"textAlign": "center"}}>
                 <h1 style={{"paddingTop": "200px", "color": "white", "position": "absolute", "margin": "auto", "left": 0, "right": 0}}>SRTP项目组题目测试</h1>
             </div>
-            <div className="buttons">
-                <Button variant="contained" style={{"marginRight": "20%"}}>安卓</Button>
-                <Button variant="contained">苹果</Button>
+            <div className="buttons" style={{"textAlign": "center"}}>
+                <Button variant="contained" style={{"marginRight": "10%"}}>安卓</Button>
+                <Button variant="contained" style={{"marginRight": "10%"}}>苹果</Button>
+                <Button variant="contained">网页</Button>
             </div>
             <div className="qrcode">
                 
@@ -33,6 +43,21 @@ function Home() {
                 <h1>{contentTitle}</h1>
             </div>
         </div>
+        <Dialog open={openLogin}>
+            <DialogTitle>
+                登录
+            </DialogTitle>
+            <DialogContent>
+                <DialogContentText>您好，请您登录SRTP账号！</DialogContentText>
+                <TextField fullWidth style={{"marginTop": "10px", "marginBottom": "10px"}} label="手机号" />
+                <TextField fullWidth label="密码" />
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handlecloseLogin}>取消</Button>
+                <Button>进入注册</Button>
+                <Button>登录</Button>
+            </DialogActions>
+        </Dialog>
         </>
     )
 }
