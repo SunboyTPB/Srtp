b3c4b9018bc7ae608d9c36a997a8d78bcc2d2c27
SunboyTPB <107842414+SunboyTPB@users.noreply.github.com>
2023-11-23 23:38:00 +0800

    main

---
+++ src/pages/Signup.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Theme.css";
+
+function Signup() {
+    return (
+        <>
+        <div style={{"height": "100vh", "backgroundColor": "#693D3D"}}>
+            
+        </div>
+        </>
+    )
+}
+
+export default Signup;
--- src/pages/ValidateLogin.js
+++ src/pages/ValidateLogin.js
@@ -1,7 +1,15 @@
 import React from "react";
 import "./Theme.css";
+import { Button, createTheme, ThemeProvider } from "@mui/material";
 
 function ValidateLogin() {
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: "#693d3d"
+            }
+        }
+    })
     return (
         <>
         <div style={{"fontFamily": "Siyuansong", "marginTop": "10%"}}>手机号</div>
@@ -10,8 +18,10 @@ function ValidateLogin() {
         </div>
         <div style={{"marginTop": "10px", "fontFamily": "Siyuansong"}}>验证码</div>
         <div style={{"marginTop": "10px"}}>
-            <input style={{"borderRadius": "51px", "border": "None", "paddingLeft": "10px", "paddingRight": "10px", "paddingTop": "10px", "paddingBottom": "10px", "width": "80%", "backgroundColor": "#EEEEEE"}} />
-            
+            <ThemeProvider theme={theme}>
+                <input style={{"borderRadius": "51px", "border": "None", "paddingLeft": "10px", "paddingRight": "10px", "paddingTop": "10px", "paddingBottom": "10px", "width": "60%", "backgroundColor": "#EEEEEE", "marginRight": "10px"}} />
+                <Button style={{"fontFamily": "Siyuansong"}}>获取验证码</Button>
+            </ThemeProvider>
         </div>
         <div>
             <a style={{"fontSize": "10px", "fontFamily": "Siyuansong"}} className="link">未收到验证码？</a>
